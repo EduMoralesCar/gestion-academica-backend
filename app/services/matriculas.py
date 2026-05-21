f23c05377f4847ff711ba3e0bd1e27e8f5d59844
Nicole Sobrino <sobrinoluza@gmail.com>
2026-05-21 12:08:23 -0500

    Crear servicio base de asignaciones

--- app/services/matriculas.py
+++ app/services/matriculas.py
@@ -1,7 +1,20 @@
 from sqlalchemy.orm import Session
+import uuid
 
 from .. import models
 
 
 def obtener_matricula(db: Session, matricula_id: str):
     return db.query(models.Matricula).filter(models.Matricula.id == matricula_id).first()
+
+
+def crear_matricula(db: Session, estudiante_id: str, curso_id: str):
+    nueva_matricula = models.Matricula(
+        id=str(uuid.uuid4()),
+        estudiante_id=estudiante_id,
+        curso_id=curso_id
+    )
+    db.add(nueva_matricula)
+    db.commit()
+    db.refresh(nueva_matricula)
+    return nueva_matricula
